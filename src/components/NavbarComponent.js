5a21b4dedd9c7fa745a731093a7dab2fe10c0e18
Patrick Litschel <ge49zel@mytum.de>
2022-06-05 14:15:51 +0200

    - Adjusted position of navigation elements - Replaced company name with transparent logo - Reworked color and style of buttons to look better

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,35 +1,45 @@
-import React, { Component } from "react";
-import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import {Button, Navbar, Nav, Dropdown, DropdownButton} from 'react-bootstrap'
+import './Navbar.css';
+import logo from "./Logo_Transparent.png";
 
 export default class NavbarComponent extends React.Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="/">Craftsman Done Fast</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar sticky="top" className="color-nav navbar-custom" expand="lg">
+                <div class="position-fixed ">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-
-
-                            {/* <Link to="/createJobOffer">About</Link> */}
-
-                            {/* <Nav.Link as={Link} to="/createJobOffer" >Home</Nav.Link> */}
-                            <Nav.Link href="/createJobOffer">Insert new Job Offer</Nav.Link>
-                            <Nav.Link href="#link">My Messages</Nav.Link>
-                            <NavDropdown title="Profile" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="/myJobOffer">My Job Offers</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="#link">Login</Nav.Link>
-                        </Nav>
+                        {/* <Link to="/createJobOffer">About</Link> */}
+                        {/* <Nav.Link as={Link} to="/createJobOffer" >Home</Nav.Link> */}
+                        <Nav.Link href="/createJobOffer">
+                            <Button class="btn btn-primary" size="lg">Insert New Job Offer</Button>
+                        </Nav.Link>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                </div>
 
+                <div class="position-fixed start-50 translate-middle-x">
+                    <Navbar.Brand href="/"><img src={logo} height={100} alt="Company Logo"/> </Navbar.Brand>
+                </div>
+
+                <div class="position-fixed end-0">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav.Link href="#link">
+                            <Button class="btn btn-primary" size="lg">Messages</Button>
+                        </Nav.Link>
+                        <DropdownButton title="Profile" id="basic-nav-dropdown" class="btn btn-primary" size="lg">
+                            <Dropdown.Item href="#action/3.1">My Profile</Dropdown.Item>
+                            <Dropdown.Item href="/myJobOffer">My Job Offers</Dropdown.Item>
+                            <Dropdown.Divider/>
+                            <Dropdown.Item href="#action/3.4">Logout</Dropdown.Item>
+                        </DropdownButton>
+                        <Nav.Link href="#link">
+                            <Button class="btn btn-primary" size="lg">Login</Button>
+                        </Nav.Link>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
         );
     }
 }
