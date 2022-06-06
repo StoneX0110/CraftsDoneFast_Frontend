66fa0e93cb54c15867b34d8ef87d9a66c5b1c97a
Daniel Testor <daniel.testor@tum.de>
2022-06-06 09:34:30 +0200

    [chg] navbar fix errors

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -7,35 +7,34 @@ export default class NavbarComponent extends React.Component {
     render() {
         return (
             <Navbar sticky="top" className="color-nav navbar-custom" expand="lg">
-                <div class="position-fixed ">
+                <div className="position-fixed ">
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         {/* <Link to="/createJobOffer">About</Link> */}
                         {/* <Nav.Link as={Link} to="/createJobOffer" >Home</Nav.Link> */}
                         <Nav.Link href="/createJobOffer">
-                            <Button class="btn btn-primary" size="lg">Insert New Job Offer</Button>
+                            <Button className="btn btn-primary" size="lg">Insert New Job Offer</Button>
                         </Nav.Link>
                     </Navbar.Collapse>
                 </div>
-
-                <div class="position-fixed start-50 translate-middle-x">
+                <div className="position-fixed start-50 translate-middle-x">
                     <Navbar.Brand href="/"><img src={logo} height={100} alt="Company Logo"/> </Navbar.Brand>
                 </div>
 
-                <div class="position-fixed end-0">
+                <div className="position-fixed end-0">
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav.Link href="#link">
-                            <Button class="btn btn-primary" size="lg">Messages</Button>
+                            <Button className="btn btn-primary" size="lg">Messages</Button>
                         </Nav.Link>
-                        <DropdownButton title="Profile" id="basic-nav-dropdown" class="btn btn-primary" size="lg">
+                        <DropdownButton title="Profile" id="basic-nav-dropdown" className="btn btn-primary" size="lg">
                             <Dropdown.Item href="#action/3.1">My Profile</Dropdown.Item>
                             <Dropdown.Item href="/myJobOffer">My Job Offers</Dropdown.Item>
                             <Dropdown.Divider/>
                             <Dropdown.Item href="#action/3.4">Logout</Dropdown.Item>
                         </DropdownButton>
                         <Nav.Link href="/login">
-                            <Button class="btn btn-primary" size="lg">Login</Button>
+                            <Button className="btn btn-primary" size="lg">Login</Button>
                         </Nav.Link>
                     </Navbar.Collapse>
                 </div>
