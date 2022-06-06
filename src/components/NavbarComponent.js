6a24d3ddc2676d581d02080930d711df095c9a31
Daniel Testor <daniel.testor@tum.de>
2022-06-06 11:28:03 +0200

    [add] navbar rendering depending on user status

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,15 +1,38 @@
 import React from "react";
-import {Button, Navbar, Nav, Dropdown, DropdownButton} from 'react-bootstrap'
+import { Button, Navbar, Nav, Dropdown, DropdownButton } from 'react-bootstrap'
 import './Navbar.css';
 import logo from "./Logo_Transparent.png";
 
 export default class NavbarComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+        };
+    }
+
+    componentDidMount() {
+        const user = JSON.parse(localStorage.getItem('userData'));
+        console.log(user);
+        if (user != null) {
+            this.state.loggedIn = true;
+            this.setState({loggedIn: true});
+        }
+    }
+
+    logout() {
+        localStorage.setItem('userData', null);
+        this.setState({loggedIn: false});
+        window.location = "/login";
+    }
+
     render() {
         return (
             <Navbar sticky="top" className="color-nav navbar-custom" expand="lg">
                 <div className="position-fixed ">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav"> 
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
                         {/* <Link to="/createJobOffer">About</Link> */}
                         {/* <Nav.Link as={Link} to="/createJobOffer" >Home</Nav.Link> */}
                         <Nav.Link href="/createJobOffer">
@@ -19,24 +42,28 @@ export default class NavbarComponent extends React.Component {
                 </div>
 
                 <div className="position-fixed start-50 translate-middle-x">
-                    <Navbar.Brand href="/"><img src={logo} height={100} alt="Company Logo"/> </Navbar.Brand>
+                    <Navbar.Brand href="/"><img src={logo} height={100} alt="Company Logo" /> </Navbar.Brand>
                 </div>
 
                 <div className="position-fixed end-0">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav.Link href="#link">
                             <Button className="btn btn-primary" size="lg">Messages</Button>
                         </Nav.Link>
+                        {this.state.loggedIn ? (
                         <DropdownButton title="Profile" id="basic-nav-dropdown" className="btn" size="lg">
-                            <Dropdown.Item href="#action/3.1">My Profile</Dropdown.Item>
+                            {this.state.loggedIn && <Dropdown.Item href="#action/3.1">My Profile</Dropdown.Item>}
                             <Dropdown.Item href="/myJobOffer">My Job Offers</Dropdown.Item>
-                            <Dropdown.Divider/>
-                            <Dropdown.Item href="#action/3.4">Logout</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick={() => this.logout()}>Logout</Dropdown.Item>
+                            {/* {false && <Dropdown.Item href="#action/3.4">Login</Dropdown.Item>}     */}
                         </DropdownButton>
+                        ) : (
                         <Nav.Link href="/login">
                             <Button className="btn btn-primary" size="lg">Login</Button>
                         </Nav.Link>
+                        )}
                     </Navbar.Collapse>
                 </div>
             </Navbar>
