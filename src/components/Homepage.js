2d1db639a5cfa2d2771fe851ceb5e10112e0ae19
Patrick Litschel <ge49zel@mytum.de>
2022-06-10 13:32:09 +0200

    [add] Homepage, displaying 10 most recent jobs

---
+++ src/components/Homepage.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import JobOfferOverviewComponent from "./jobOffer/JobOfferOverviewComponent";
+import axios from "axios";
+
+export default class Homepage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.jobs = "";
+        this.renderJobs = "";
+    }
+
+
+    componentDidMount() {
+        console.log("fetch job offers");
+        axios.get('/api/jobOffer/recentJobOffers').then(res => {
+            this.jobs = res
+            this.jobs = this.jobs.data.sort((a, b) => {
+                return b.insertionDate.localeCompare(a.insertionDate);
+            });
+            this.jobs.splice(10)
+            // this.renderJobs = this.jobs.map((e) => <JobOfferOverviewComponent key={e._id}>{e.title} - {e.category}</JobOfferOverviewComponent>)
+            this.renderJobs = this.jobs.map((e) => <JobOfferOverviewComponent key={e._id} job={e}/>)
+            // console.log(this.renderJobs);
+            this.forceUpdate();
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Insert searchbar here</h1>
+                <h2>Recent Jobs:</h2>
+                {this.renderJobs}
+            </div>
+        );
+    }
+}
--- src/components/OverviewView.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export default OverviewView => <div>This is home component</div>;
