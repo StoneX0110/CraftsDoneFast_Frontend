9c8686391a5c9d358b4908a5dbf1c2ad31d31cca
Patrick Litschel <ge49zel@mytum.de>
2022-06-20 16:23:35 +0200

    [chg] "Confirm Job Offer"-button relocated to the bottom right

--- src/components/jobOffer/InsertJobOfferView.js
+++ src/components/jobOffer/InsertJobOfferView.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import axios from "axios";
 import Resizer from "react-image-file-resizer";
 import './JobOffer.css'
@@ -25,7 +25,7 @@ export default class InsertJobOfferView extends Component {
 
     handleChange(event) {
         const name = event.target.name;
-        this.setState({ [name]: event.target.value });
+        this.setState({[name]: event.target.value});
     }
 
     handleSubmit = event => {
@@ -52,7 +52,7 @@ export default class InsertJobOfferView extends Component {
                     this.state.images.push(uri);
                     this.imageURLs = this.state.images.map(imageSrc => <ImageComponent imageSrc={imageSrc}/>);
                     // <span><div className="photo border border-1 mb-3"><img className="image" key={imageSrc} src={imageSrc} /></div></span>);
-                    this.setState({ imageURLs: this.imageURLs });
+                    this.setState({imageURLs: this.imageURLs});
                 },
                 "base64",
                 10,
@@ -64,51 +64,61 @@ export default class InsertJobOfferView extends Component {
     }
 
 
-
     render() {
         return (
             <div className="col-md-9 jobOffer">
                 <p className="h1">Insert a Job Offer</p>
                 <form onSubmit={this.handleSubmit}>
-                    <button type="submit" className="btn btn-success">Confirm Job Offer</button>
                     <div className="form-group">
                         <label>Title</label>
-                        <input required type="text" name="title" className="form-control" id="exampleFormControlInput1" value={this.state.title} onChange={this.handleChange}
-                            placeholder="Insert Title..." />
+                        <input required type="text" name="title" className="form-control" id="exampleFormControlInput1"
+                               value={this.state.title} onChange={this.handleChange}
+                               placeholder="Insert Title..."/>
                     </div>
                     <div className="form-row row">
                         <div className="form-group col-md-4">
                             <label>Category</label>
-                            <select required name="category" className="form-control" id="exampleFormControlSelect1" value={this.state.category} onChange={this.handleChange}>
+                            <select required name="category" className="form-control" id="exampleFormControlSelect1"
+                                    value={this.state.category} onChange={this.handleChange}>
                                 <option defaultValue disabled value="">Choose...</option>
                                 {Category.returnSelection()}
                             </select>
                         </div>
                         <div className="form-group col-md-4">
                             <label>Postal Code</label>
-                            <input name="postalCode" type="text" className="form-control" id="exampleFormControlInput1" value={this.state.postalCode}
-                                onChange={this.handleChange} placeholder="Insert Postal Code" />
+                            <input required name="postalCode" type="text" className="form-control"
+                                   id="exampleFormControlInput1" value={this.state.postalCode}
+                                   onChange={this.handleChange} placeholder="Insert Postal Code"/>
                         </div>
                         <div className="form-group col-md-4">
                             <label>Price expectation*</label>
-                            <input name="priceExpectation" type="text" className="form-control" id="exampleFormControlInput1" value={this.state.priceExpectation}
-                                onChange={this.handleChange} placeholder="Price expectation" />
+                            <input name="priceExpectation" type="text" className="form-control"
+                                   id="exampleFormControlInput1" value={this.state.priceExpectation}
+                                   onChange={this.handleChange} placeholder="Price expectation"/>
                         </div>
                     </div>
                     <div className="form-group">
                         <label>Description</label>
-                        <textarea name="description" className="form-control" id="exampleFormControlTextarea1" rows="5" value={this.state.description}
-                            onChange={this.handleChange}></textarea>
+                        <textarea required name="description" className="form-control" id="exampleFormControlTextarea1"
+                                  rows="5" value={this.state.description}
+                                  onChange={this.handleChange}></textarea>
                     </div>
-                    <div  className="from-group col-md-4">
-                        <label>Insert Pictures*</label>
-                        <input className="form-control" type="file" multiple accept="image/*" onChange={this.onImageChange} />
-                        <div>
-                            {this.state.imageURLs}
+                    <div className="form-row row">
+                        <div className="from-group col-md-4">
+                            <label>Insert Pictures*</label>
+                            <input className="form-control" type="file" multiple accept="image/*"
+                                   onChange={this.onImageChange}/>
+                            <div>
+                                {this.state.imageURLs}
+                            </div>
+                        </div>
+                        <div className="from-group col-md-4"></div>
+                        <div className="from-group col-md-4">
+                            <button type="submit" className="btn btn-success float-end">Confirm Job Offer</button>
                         </div>
                     </div>
                 </form>
-            </div >
+            </div>
         );
     }
 }
