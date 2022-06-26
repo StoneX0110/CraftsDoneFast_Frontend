25970e7031ebc43de070a08d961e496551b09bbc
Florian Stang <ge25jac@mytum.de>
2022-06-26 15:10:35 +0200

    [chg] popup to use new external component, fixed bug which made it necessary to be logged in to view craftsman profiles. (in cooperation with Patrick and Jannis)

--- src/components/user/UserView.js
+++ src/components/user/UserView.js
@@ -1,6 +1,6 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import Select from 'react-select'
-import Popup from 'reactjs-popup';
+import Popup from './PopupCreateChat';
 import 'reactjs-popup/dist/index.css';
 import axios from "axios";
 import "./UserView.css"
@@ -28,8 +28,6 @@ export default class UserView extends Component {
         this.jobNames = [];
         this.handleChange = this.handleChange.bind(this);
         this.updateUser = this.updateUser.bind(this);
-        this.handleSelectionChange = this.handleSelectionChange.bind(this);
-        this.handleChatCreation = this.handleChatCreation.bind(this)
         this.render = this.render.bind(this);
         if (sessionStorage.getItem('userData') && JSON.parse(sessionStorage.getItem('userData')) !== null) {
             this.user = JSON.parse(sessionStorage.getItem('userData')).username;
@@ -44,12 +42,6 @@ export default class UserView extends Component {
 
         //fill options
         this.options = Category.returnOptions();
-        axios.get('/api/jobOffer/myJobOffers').then(res => {
-            this.jobs =  res.data;
-            console.log(this.jobs)
-            this.jobNames = this.jobs.map((e) => e.title);
-            this.forceUpdate();
-        }).then()
     }
 
     handleChange(event) {
@@ -80,63 +72,20 @@ export default class UserView extends Component {
         });
     }
 
-    handleChatCreation() {
-        console.log("test chat creation")
-        console.log(this.state)
-        console.log("Title: " + this.state.title)
-        console.log("Job: " + this.state.job)
-    }
-
-    returnSelected() {
-        return this.jobNames.map((e) => <option>{e}</option>)
-    }
-
     render() {
         const def = this.state.skills;
         return (
             <div className="profile">
                 <p className="h1">Profile</p>
                 {this.user === this.username && !this.state.edit &&
-                    <button type="button" className="btn btn-primary" onClick={(e) => this.setState({edit: true})}>
-                        Edit Profile
-                    </button>}
+                <button type="button" className="btn btn-primary" onClick={(e) => this.setState({edit: true})}>
+                    Edit Profile
+                </button>}
                 {this.user === this.username && this.state.edit &&
-                    <button type="button" className="btn btn-info" onClick={this.updateUser}>Save Profile</button>}
+                <button type="button" className="btn btn-info" onClick={this.updateUser}>Save Profile</button>}
                 {/*TODO: CSS für Schönheit noch definieren*/}
                 {this.user !== this.username && (this.state.skills.length > 0) &&
-                <Popup
-                    trigger={<button type="button" className="btn btn-primary"> Contact </button>}
-                    modal
-                >
-                    {close => (
-                        <div>
-                            <button className="close" onClick={close}>
-                                &times;
-                            </button>
-                            <div className="header"> Contact {this.state.name} </div>
-                            <label>Select Job Offer</label>
-                            <select required name="job" className="form-control" id="job"
-                                    value={this.state.job} onChange={this.handleChange}>
-                                <option defaultValue disabled value="">Choose...</option>
-                                <option>Any</option>
-                                {this.returnSelected()}
-                            </select>
-                            <div className="form-group">
-                                <label>Title</label>
-                                <input required type="text" name="title" className="form-control" id="exampleFormControlInput1"
-                                       value={this.state.title} onChange={this.handleChange}
-                                       placeholder="Insert Title..."/>
-                            </div>
-                            <div>
-                                <button type="button" /*className="btn btn-primary"*/ onClick={() => {
-                                    console.log('modal closed ');
-                                    close();
-                                    this.handleChatCreation();
-                                }}>Contact</button>
-                            </div>
-                        </div>
-                    )}
-                </Popup>
+                    <Popup username={this.state.name}/>
                 }
                 <div className="form-group settings">
                     <div className="form-row row">
@@ -168,14 +117,14 @@ export default class UserView extends Component {
                     <div className="form-row row">
                         Skills : {this.displaySelected(this.state.skills)}
                         {this.user === this.username && this.state.edit &&
-                            <Select className="basic-multi-select"
-                                    closeMenuOnSelect={false}
-                                    classNamePrefix="select"
-                                    value={this.state.skills}
-                                    options={this.options}
-                                    isMulti={true}
-                                    onChange={this.handleSelectionChange}
-                            />
+                        <Select className="basic-multi-select"
+                                closeMenuOnSelect={false}
+                                classNamePrefix="select"
+                                value={this.state.skills}
+                                options={this.options}
+                                isMulti={true}
+                                onChange={this.handleSelectionChange}
+                        />
                         }
                     </div>
                 </div>
