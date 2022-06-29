1a4cc433aba148f1f925967d4ccfc718170eed31
Florian Stang <ge25jac@mytum.de>
2022-06-29 16:13:28 +0200

    [chg] accept contract popup look to use popup.css

--- src/components/chat/AcceptContractPopup.js
+++ src/components/chat/AcceptContractPopup.js
@@ -27,17 +27,17 @@ export function AcceptContractPopup(props) {
         <div>
             <Button border onClick={handleOpen}>Accept Contract</Button>
             <Popup open={open} closeOnDocumentClick onClose={handleClose}>
-                <div>
+                <div className="popupMainContainer">
                     <button className="close" onClick={handleClose}>
                         &times;
                     </button>
                     <div className="header">Do you wish to accept the Contract Details?</div>
-                    <div>
+                    <div className="popupInputContainer">
                         <h2>Price: {price}</h2>
                         <h2>Starting Date: {startingDate}</h2>
                     </div>
-                    <div>
-                        <button type="button" /*className="btn btn-primary"*/ onClick={() => {
+                    <div className="popupButtonContainer">
+                        <button type="button" className="btn popupButton"/*className="btn btn-primary"*/ onClick={() => {
                             handleClose();
                             acceptContract();
                         }}>Accept Contract Details
