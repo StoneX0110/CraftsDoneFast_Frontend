a9d2a1af6875dcb5365ae590e40525dca223c4f3
Jannis Bittmayer <ge49foh@mytum.de>
2022-07-01 16:12:23 +0200

    [add] live chat functionality [rm] placeholders in HTML

--- src/components/chat/ChatView.js
+++ src/components/chat/ChatView.js
@@ -19,6 +19,7 @@ import {AcceptContractPopup} from "./AcceptContractPopup";
 import {ConfirmJobCompletionPopup} from "./ConfirmJobCompletionPopup";
 import {RatingPopup} from "./RatingPopup";
 import axios from "axios";
+import {io} from "socket.io-client";
 
 export function ChatView() {
 
@@ -42,25 +43,32 @@ export function ChatView() {
     const inputRef = useRef();
     const [msgInputValue, setMsgInputValue] = useState("");
     const [messages, setMessages] = useState([]);
+    //test states are: "noPayment", "openContract", "contractEstablished", "paymentDone", "jobCompleted"
     const [contractState, setContractState] = useState("noPayment");
     //chatscope.io conversations
     const [conversations, setConversations] = useState([]);
     const [chats, setChats] = useState([]);
     const [activeChatId, setActiveChatId] = useState('');
+    //added because activeChatId state could not be accessed from useEffect
+    const activeChatIdRef = useRef('');
 
-    //test states are: "noPayment", "openContract", "contractEstablished", "paymentDone", "jobCompleted"
+    //initialize socket for live chat as null
+    const socket = useRef(null);
 
-    //gets chats & messages; creates conversations bar; called once when loading the page
+    //gets chats & messages; creates conversations bar
     function getChats() {
         //get chats with messages from db
         axios.get('/api/chat/getMyChats').then(res => {
             //only continue if conversations are not set already
             if (conversations.length === 0) {
                 //create chatscope.io Conversation UI components from received chats
+                //TODO: mark active chat; hint: Conversation can have 'active' attribute
                 let chatTemp = res.data.map(chatWithPartner => {
-                    return <Conversation name={chatWithPartner.chat.title} info={"Chat partner: " + chatWithPartner.partnerUsername}
+                    return <Conversation name={chatWithPartner.chat.title} active={false}
+                                         info={"Chat partner: " + chatWithPartner.partnerUsername}
                                          onClick={() => {
                                              setActiveChatId(chatWithPartner.chat._id);
+                                             activeChatIdRef.current = chatWithPartner.chat._id;
                                          }}>
                         <Avatar src={"defaultAvatar.png"} name={chatWithPartner.partnerUsername}/>
                         <Conversation.Operations
@@ -74,12 +82,49 @@ export function ChatView() {
         })
     }
 
-    //display messages of other chat when active chat is changed
+    //create websocket, connect to rooms & handle received messages
+    const chatCount = useRef(0);
     useEffect(() => {
-        //don't load anything on activeChat initialization
-        if (activeChatId === '') return;
+        //check so execution only happens when chats are loaded from database
+        if (chats.length !== chatCount.current) {
+            chatCount.current = chats.length;
+            setActiveChatId(chats[0].chat._id);
+            activeChatIdRef.current = chats[0].chat._id;
+            //once chats are loaded, create websocket
+            socket.current = io("ws://localhost:3002");
+            //join room for each chat
+            chats.forEach(chatOb => {
+                socket.current.emit("create", chatOb.chat._id);
+            })
+            //set up code to execute when message is received
+            socket.current.on("receiveMessage", (message) => {
+                //write message to local copy of chats
+                console.log('receive: ' + message.content);
+                console.log(chats);
+                let newChats = chats;
+                newChats.map(chatWithPartner => {
+                    if (chatWithPartner.chat._id === message.chat) {
+                        chatWithPartner.chat.messages.push(message);
+                    }
+                    return chatWithPartner;
+                });
+                setChats(newChats);
+                //if chat of incoming message is active, show in UI
+                if (message.chat === activeChatIdRef.current) {
+                    updateActiveChat(message.chat, newChats);
+                }
+            })
+        }
+    }, [chats])
+
+    /*
+        updates active chat messages using id of chat to update messages from.
+        second parameter is optional chat object when calling from a place where "chats" state might not be up-to-date
+     */
+    function updateActiveChat(activeId, newChats = null) {
+        if (newChats === null) newChats = chats;
         //find chat with id of active chat from local storage
-        let chatToLoad = chats.find(chatWithPartner => chatWithPartner.chat._id === activeChatId);
+        let chatToLoad = newChats.find(chatWithPartner => chatWithPartner.chat._id === activeId);
         //create message objects for frontend framework
         let tempMessages = [];
         chatToLoad.chat.messages.forEach(message => {
@@ -97,6 +142,12 @@ export function ChatView() {
         setMessages(tempMessages.sort(
             (objA, objB) => Number(objA.sentTime) - Number(objB.sentTime),
         ));
+    }
+
+    //display messages of other chat when active chat is changed
+    useEffect(() => {
+        //don't load anything on activeChat initialization
+        if (activeChatId !== '') updateActiveChat(activeChatId);
     }, [activeChatId])
 
     //called when message is sent from UI
@@ -128,6 +179,9 @@ export function ChatView() {
             return chatWithPartner;
         })
         setChats(newChats);
+
+        //send message using websocket for live chat
+        socket.current.emit("sendMessage", tempMessage);
     };
 
     return (
@@ -136,12 +190,7 @@ export function ChatView() {
             <div className="chatContainer">
                 <MainContainer>
                     <Sidebar position="left" scrollable={false}>
-                        <Search placeholder="Search..."/>
                         <ConversationList>
-                            <Conversation name="Lilly" lastSenderName="Lilly" info="Yes i can do it for you">
-                                <Avatar src={"defaultAvatar.png"} name="Lilly" status="available"/>
-                                <Conversation.Operations onClick={() => console.log('Operations clicked Lilly')}/>
-                            </Conversation>
                             {getChats()}
                             {conversations}
                         </ConversationList>
