759fe867b8f60b7f30a7bdba85bac5122cc57863
Florian Stang <ge25jac@mytum.de>
2022-07-27 16:15:19 +0200

    [add] stripeID field to AcceptContractPopup case craftsmen did not provide one in his profile settings

--- src/components/chat/AcceptContractPopup.js
+++ src/components/chat/AcceptContractPopup.js
@@ -6,13 +6,16 @@ import Moment from "moment";
 
 export function AcceptContractPopup(props) {
     let user = '';
+    let currentStripeID = '';
     if (sessionStorage.getItem('userData') && JSON.parse(sessionStorage.getItem('userData')) !== null) {
         user = JSON.parse(sessionStorage.getItem('userData')).username;
+        currentStripeID = JSON.parse(sessionStorage.getItem('userData')).settings.stripeID;
     }
 
     const [open, setOpen] = useState(false);
     const [price, setPrice] = useState("");
     const [startingDate, setStartingDate] = useState("");
+    const [stripeID, setStripeID] = useState("");
     const handleOpen = () => {
         setOpen(true);
         setPrice(props.contract.price)
@@ -20,9 +23,26 @@ export function AcceptContractPopup(props) {
     }
     const handleClose = () => setOpen(false);
 
+    function handleChangeStripeID(event) {
+        setStripeID(event.target.value);
+    }
+
     function acceptContract() {
         let state = JSON.parse(JSON.stringify(props.contract));
         state.paymentStatus = 'contractEstablished';
+        if (stripeID !== "") {
+            user = {
+                stripeID : stripeID,
+                id: JSON.parse(sessionStorage.getItem('userData')).id,
+            }
+            axios.post('/api/user/updateUserStripeID', user)
+            let sessionData = sessionStorage.getItem('userData');
+            let stringArray = sessionData.split('"stripeID":');
+            sessionData = stringArray[0] + '"stripeID":';
+            let tempData = JSON.parse(JSON.stringify(stripeID));
+            sessionData += JSON.stringify(tempData) + '}}';
+            sessionStorage.setItem('userData', sessionData);
+        }
         axios.post('/api/chat/updateContract', state)
             .then(res => {
                 props.sendSystemMessage('<Message.CustomContent>' +
@@ -33,6 +53,12 @@ export function AcceptContractPopup(props) {
                     '<span style="color:darkred">' + startingDate + '</span>' +
                     '</Message.CustomContent>', state);
             })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        acceptContract();
+        handleClose();
     }
 
     return (
@@ -47,24 +73,29 @@ export function AcceptContractPopup(props) {
                 <Modal.Header>
                     <Modal.Title>Do you wish to accept the Contract Details?</Modal.Title>
                 </Modal.Header>
-                <Modal.Body className="popupMainContainer">
-                    <div className="popupInputContainer">
-                        <div className="card-text col-auto text-center">
-                            <label>Starting Date: {startingDate}</label>
+                <form onSubmit={handleSubmit}>
+                    <Modal.Body className="popupMainContainer">
+                        <div className="popupInputContainer">
+                            <div className="card-text col-auto text-center">
+                                <label>Starting Date: {startingDate}</label>
+                            </div>
+                            <div className="card-text col-auto text-center">
+                                <label>Price: {price}$</label>
+                            </div>
+                            {currentStripeID === "" &&
+                            <label>Payment Destination</label> &&
+                            <input required type="string" name="stripeID" className="form-control"
+                                   value={stripeID} onChange={handleChangeStripeID}
+                                   id="stripeInput"
+                                   placeholder="Insert your StripeID"/>
+                            }
                         </div>
-                        <div className="card-text col-auto text-center">
-                            <label>Price: {price}$</label>
-                        </div>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button type="cancel" className="btn popupButtonCancel" onClick={handleClose}>Cancel</button>
-                    <button type="button" className="btn popupButton"/*className="btn btn-primary"*/ onClick={() => {
-                        handleClose();
-                        acceptContract();
-                    }}>Accept Contract Details
-                    </button>
-                </Modal.Footer>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button type="button" className="btn popupButtonCancel" onClick={handleClose}>Cancel</button>
+                        <button id="submit" type="submit" className="btn popupButton">Accept Contract Details</button>
+                    </Modal.Footer>
+                </form>
             </Modal>
         </div>
     );
