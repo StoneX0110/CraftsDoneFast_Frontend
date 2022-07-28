e710ad5eb3a4d1e66e2a087b77075335caab7d4f
Florian Stang <ge25jac@mytum.de>
2022-07-28 11:38:21 +0200

    [chg] payment method process

--- src/components/payment/PaymentComponent.js
+++ src/components/payment/PaymentComponent.js
@@ -6,33 +6,44 @@ import {
 } from "@stripe/react-stripe-js";
 import {loadStripe} from "@stripe/stripe-js";
 import axios from "axios";
-import React from "react";
+import React, {useState} from "react";
 
 const stripePromise = loadStripe("pk_test_51LFv4cGPYqiDG82LLOGXRipIBm2gDVmi6MZyJKXNDSW5PpFXAiBBdAd1pFa0wGdaytWY3cOPhyl8grV4lybRknCd00R0lvsMPY");
 
-const handleSubmit = (stripe, elements, props) => async () => {
-    const cardElement = elements.getElement(CardElement);
-    //card number has to be 4242 4242 4242 4242, postalcode 5 numbers, validation date in future, ccv 3 digits
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
-    });
-
-    if (error) {
-        console.log('[error]', error);
-    } else {
-        // ... SEND to your API server to process payment intent
-        confirm(paymentMethod, props);
-    }
-};
-
-function confirm(paymentMethod, props) {
-    sendToServer(props, paymentMethod);
+const handleSubmit = (stripe, elements, props, setPayed) => async () => {
+    setPayed(true);
+    axios.post('/api/payment/createPaymentIntent', {
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(props.contract),
+    }).then(
+        res => {
+            confirmPayment(stripe, elements, res.data.clientSecret, props, setPayed);
+        });
 }
 
-function sendToServer(props, paymentMethod) {
+const confirmPayment = (stripe, elements, clientSecret, props, setPayed) => {
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: elements.getElement(CardElement),
+        }
+    }).then(res => {
+        console.log(res)
+        if (res.error) {
+            setPayed(false);
+            console.log('[error]', res.error);
+        } else {
+            confirm(props);
+        }
+    });
+}
+
+function confirm(props) {
+    sendToServer(props);
+}
+
+function sendToServer(props) {
     if(props.profile) {
-        axios.post('/api/user/payProfile', paymentMethod)
+        axios.post('/api/user/payProfile')
         .then(res => {
             alert(res.data);
             props.handleClose();
@@ -57,12 +68,13 @@ function sendToServer(props, paymentMethod) {
 const PaymentForm = (props) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [payed, setPayed] = useState(false);
     return (
         <>
             <CardElement options={{hidePostalCode: true}}/>
             <div className="popupButtonContainer">
                 <button type="cancel" className="btn popupButtonCancel" onClick={props.handleClose}>Cancel</button>
-                <button type="button" className="btn popupButton" onClick={handleSubmit(stripe, elements, props)}>Pay
+                <button type="button" disabled={payed} className="btn popupButton" onClick={handleSubmit(stripe, elements, props, setPayed)}>Pay
                     with Stripe
                 </button>
             </div>
