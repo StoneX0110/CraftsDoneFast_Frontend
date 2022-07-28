bff2cc70a322f548c828622b2d41c8997495f9ed
Florian Stang <ge25jac@mytum.de>
2022-07-28 15:45:03 +0200

    [fixed] signup with empty field and removed redirection if username is already in use.

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import axios from "axios";
+
 export default class Registration extends Component {
 
     constructor(props) {
@@ -10,22 +11,26 @@ export default class Registration extends Component {
             repeatPassword: "",
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         const name = event.target.name;
-        this.setState({ [name]: event.target.value });
+        this.setState({[name]: event.target.value});
     }
 
 
-
-    register() {
+    handleSubmit= event => {
+        event.preventDefault();
+        console.log("Test");
         axios.post('/api/auth/signup', this.state)
             .then(res => {
-                window.location = "/login";
+                if (res) {
+                    window.location = "/login";
+                }
             }).catch(error => {
-                console.log(error);
-            })
+            console.log(error);
+        });
     }
 
     render() {
@@ -38,11 +43,15 @@ export default class Registration extends Component {
                                 <div className="card shadow-2-strong card-registration">
                                     <div className="card-body p-4 p-md-5">
                                         <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
+                                        <form onSubmit={this.handleSubmit}
+                                              style={{marginRight: "20px", marginLeft: "20px"}}>
                                             <div className="row">
                                                 <div className="col-md-12 mb-4">
 
                                                     <div className="form-outline">
-                                                        <input type="text" name="username" value={this.state.username} onChange={this.handleChange} className="form-control form-control-lg" />
+                                                        <input type="text" name="username" value={this.state.username}
+                                                               onChange={this.handleChange}
+                                                               className="form-control form-control-lg"/>
                                                         <label className="form-label">Enter Username</label>
                                                     </div>
 
@@ -50,20 +59,24 @@ export default class Registration extends Component {
                                                 <div className="col-md-12 mb-4">
 
                                                     <div className="form-outline">
-                                                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange} className="form-control form-control-lg" />
+                                                        <input type="password" name="password"
+                                                               value={this.state.password} onChange={this.handleChange}
+                                                               className="form-control form-control-lg" required/>
                                                         <label className="form-label">Enter Password</label>
                                                     </div>
                                                     <div className="form-outline">
-                                                        <input type="password" name="repeatPassword" value={this.state.repeatPassword} onChange={this.handleChange} className="form-control form-control-lg" />
+                                                        <input type="password" name="repeatPassword"
+                                                               value={this.state.repeatPassword}
+                                                               onChange={this.handleChange}
+                                                               className="form-control form-control-lg" required/>
                                                         <label className="form-label">Re-enter Password</label>
                                                     </div>
-
-
                                                 </div>
-                                            </div>                                                                           
-                                            <div className="mt-0 pt-2">
-                                                <button className="btn btn-success btn-lg" onClick={() => this.register()}>Register</button>
                                             </div>
+                                            <div className="mt-0 pt-2">
+                                                <button type="submit" className="btn btn-success btn-lg">Register</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
